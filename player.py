21d323ccde8b2b7e167b884613aa89cc190e7cd4
Johannes Schirrmeister <johannes.schirrmeister@prezi.com>
2015-04-25 10:22:08 +0200

    Implement is_pair

--- player.py
+++ player.py
@@ -8,3 +8,6 @@ class Player:
     def showdown(self, game_state):
         pass
 
+
+def is_pair(hole_cards):
+    return hole_cards[0]["rank"] == hole_cards[1]["rank"]
--- test.py
+++ test.py
@@ -41,5 +41,13 @@ class PlayerTest(unittest.TestCase):
     def test_betRequest_type_returned_integer(self):
         self.assertEqual(type(self.player.betRequest(self.game_state)), int)
 
+    def test_is_pair(self):
+        hole_cards_with_pair = [dict(rank="6", suit="hearts"), dict(rank="6", suit="spades")]
+
+        self.assertTrue(is_pair(hole_cards_with_pair))
+
+        hole_cards_without_pair = [dict(rank="6", suit="hearts"), dict(rank="K", suit="spades")]
+        self.assertFalse(is_pair(hole_cards_without_pair))
+
 if __name__ == "__main__":
     unittest.main()
