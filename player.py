253b091b0867d98ef6c5c58a713081ed22b70bce
Liana Lo <liana.lo@prezi.com>
2015-04-25 10:50:39 +0200

    more card rankings

--- player.py
+++ player.py
@@ -1,3 +1,7 @@
+SHITTY_CARDS = ['2', '3', '4', '5', '6']
+OKAY_CARDS = ['7', '8', '9', '10']
+GOOD_CARDS = ['A', 'K', 'Q', 'J']
+
 class Player:
     VERSION = "NO FOLDS LOL"
 
@@ -6,10 +10,13 @@ class Player:
         hole_cards = game_state['players'][player_index]['hole_cards']
         ranks = [c['rank'] for c in hole_cards]
 
+        total_bet = 0
         if includes_high_symbol(ranks) or is_pair(hole_cards):
             print '##### hole hand A'
-            return 1000
-        return 0
+            total_bet += 1000
+        if includes_okay_cards(ranks):
+            total_bet += 500
+        return total_bet
 
     def showdown(self, game_state):
         pass
@@ -19,14 +26,15 @@ def is_pair(hole_cards):
 
 
 def includes_symbol(ranks):
-    symbols = ['A', 'K', 'Q', 'J']
-    return any(r in symbols for r in ranks)
+    return any(r in GOOD_CARDS for r in ranks)
 
 
 def includes_high_symbol(ranks):
     symbols = ['A', 'K']
     return any(r in symbols for r in ranks)
 
+def includes_okay_cards(ranks):
+    return any(r in OKAY_CARDS for r in ranks)
 
 def get_call_value(game_state):
     player_index = game_state['in_action']
