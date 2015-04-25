323f7fae08b98cbd2c798a963a18ceb52970d1e8
Liana Lo <liana.lo@prezi.com>
2015-04-25 10:31:59 +0200

    get call and get min. raise

--- player.py
+++ player.py
@@ -17,3 +17,9 @@ class Player:
 
 def is_pair(hole_cards):
     return hole_cards[0]["rank"] == hole_cards[1]["rank"]
+
+def get_call_value(game_state):
+    return current_buy_in - players[in_action][bet]
+
+def get_minimum_raise_value(game_state):
+    return current_buy_in - players[in_action][bet] + minimum_raise
