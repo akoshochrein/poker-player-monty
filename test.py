f77e3e70234e3a2605ae7c117572eb2706856fe4
Liana Lo <liana.lo@prezi.com>
2015-04-25 10:10:52 +0200

    working test

--- test.py
+++ test.py
@@ -3,38 +3,43 @@ from player import *
 
 class PlayerTest(unittest.TestCase):
     def setUp(self):
-        game_state = """{
-  "players":[
-    {
-      "name":"Player 1",
-      "stack":1000,
-      "status":"active",
-      "bet":0,
-      "hole_cards":[],
-      "version":"Version name 1",
-      "id":0
-    },
-    {
-      "name":"Player 2",
-      "stack":1000,
-      "status":"active",
-      "bet":0,
-      "hole_cards":[],
-      "version":"Version name 2",
-      "id":1
-    }
-  ],
-  "tournament_id":"550d1d68cd7bd10003000003",
-  "game_id":"550da1cb2d909006e90004b1",
-  "round":0,
-  "bet_index":0,
-  "small_blind":10,
-  "orbits":0,
-  "dealer":0,
-  "community_cards":[],
-  "current_buy_in":0,
-  "pot":0
-}"""
+        self.game_state = """{
+                              "players":[
+                                {
+                                  "name":"Player 1",
+                                  "stack":1000,
+                                  "status":"active",
+                                  "bet":0,
+                                  "hole_cards":[],
+                                  "version":"Version name 1",
+                                  "id":0
+                                },
+                                {
+                                  "name":"Player 2",
+                                  "stack":1000,
+                                  "status":"active",
+                                  "bet":0,
+                                  "hole_cards":[],
+                                  "version":"Version name 2",
+                                  "id":1
+                                }
+                              ],
+                              "tournament_id":"550d1d68cd7bd10003000003",
+                              "game_id":"550da1cb2d909006e90004b1",
+                              "round":0,
+                              "bet_index":0,
+                              "small_blind":10,
+                              "orbits":0,
+                              "dealer":0,
+                              "community_cards":[],
+                              "current_buy_in":0,
+                              "pot":0
+                            }"""
+
+        self.player = Player()
 
     def test_betRequest(self):
-        self.assertEqual(typeof(betRequest(game_state)), int)
+        self.assertEqual(type(self.player.betRequest(self.game_state)), int)
+
+if __name__ == "__main__":
+    unittest.main()
